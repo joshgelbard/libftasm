30c694bb1e49e5bf5750c5a02d6b034e26c41871
Josh Gelbard <jgelbard@student.42.us.org>
2018-11-15 15:32:49 -0800

    Style and clarity changes in ft_puts

--- ft_puts.s
+++ ft_puts.s
@@ -3,43 +3,43 @@
 
 	section	.text
 _ft_puts:
-	xor		rdx, rdx ; rdx = buf len for write
-	cmp		rdi, 0x0 ; special case: puts(NULL) prints "(null)"
+	xor		rdx, rdx ; buf len
+	mov		rsi, rdi ; buf addr
+	mov		edi, 1 ; fd (must set AFTER mov buf addr)
+	cmp		rsi, 0x0 ; special case: ft_puts(NULL) prints "(null)"
 	jne		.get_len
-	lea		rsi, [rel strnull] ; rsi = buf ptr
+	lea		rsi, [rel strnull]
 	mov		rdx, 6
 	jmp		.writestr
 .get_len:
-	mov		rsi, rdi ; relocate buf ptr for write
 	cmp		byte [rsi+rdx], 0
 	jz		.writestr
 	inc		rdx
 	jmp		.get_len
 .writestr:
-	mov		rax, 0x02000004
-	mov		rdi, 1 ; stdout
+	mov		eax, 0x02000004 ; write
 	syscall
-	jc		.fail
+	jc		.fail ; carry flag = syscall failed
 .writenl:
-	mov		rax, 0x02000004 ; write syscall has clobbered rax
+	mov		eax, 0x02000004 ; write syscall has clobbered rax
 	lea		rsi, [rel newline]
-	mov		edi, 1
 	mov		rdx, 1
 	syscall
 	jc		.fail ; syscalls signal fail with carry flag
 	ret ; return any nonnegative on success is ok per man page
 .fail:
-	mov		rax, -1
+	mov		eax, -1 ; return EOF
 	ret
 
 	section	.data
 newline:	db `\n`
 strnull:	db '(null)'
 
-; write syscall info:
-; rax: 4 (number of write syscall)
-; rdi: file descriptor
-; rsi: address of buf
-; rdx: length of buf
-; on success, carry flag will not be set and rax will hold length of buf
-; on error, carry flag will be set and rax will hold errno
+; setting up for write syscall:
+;   eax: 0x02000004 (number of write syscall)
+;   edi: file descriptor
+;   rsi: address of buf
+;   rdx: length of buf
+; after the call:
+;   on success, carry flag will not be set and eax will be nonnegative
+;   on error, carry flag will be set and eax will hold errno
