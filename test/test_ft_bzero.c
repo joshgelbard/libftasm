7fe0b3480e5b0322205d7913e2a863bec019d929
Josh Gelbard <jgelbard@student.42.us.org>
2018-11-16 20:24:34 -0800

    Beautify ft_bzero test

--- test/test_ft_bzero.c
+++ test/test_ft_bzero.c
@@ -1,29 +1,36 @@
 #include "testrunner.h"
 
+static void test_once(char *s)
+{
+	size_t sz = strlen(s) + 1;
+	char *s1 = strdup(s);
+	char *s2 = strdup(s);
+
+	bzero(s1, 0);
+	ft_bzero(s2, 0);
+	assert(!memcmp(s1, s2, sz));
+
+	bzero(s1, sz - 1);
+	ft_bzero(s2, sz - 1);
+	assert(!memcmp(s1, s2, sz));
+
+	bzero(s1, sz);
+	ft_bzero(s2, sz);
+	assert(!memcmp(s1, s2, sz));
+
+	free(s1);
+	free(s2);
+}
+
 void	test_ft_bzero(void)
 {
-	char *ss[] = {
-		"",
-		".",
-		"..",
-		"...................................."
-	};
-	size_t nelems = (sizeof(ss) / sizeof(*(ss)));
-	for (size_t i = 0; i < nelems; i++) {
-		size_t sz = strlen(ss[i]) + 1;
-		char *s = strdup(ss[i]);
-		char *s2 = strdup(ss[i]);
-		bzero(s, 0);
-		ft_bzero(s2, 0);
-		assert(!memcmp(s, s2, sz));
-		bzero(s, sz - 1);
-		ft_bzero(s2, sz - 1);
-		assert(!memcmp(s, s2, sz));
-		bzero(s, sz);
-		ft_bzero(s2, sz);
-		assert(!memcmp(s, s2, sz));
-		free(s);
-		free(s2);
-	}
+	test_once("");
+	test_once(".");
+	test_once("..");
+	test_once(".....................");
+	char *bigstring = calloc(99999, 1);
+	memset(bigstring, '.', 99998);
+	test_once(bigstring);
+	free(bigstring);
 	printf("%s: ok\n", __func__);
 }
